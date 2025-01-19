be38a35e9dc1da4958622f7c4ef25a5ba1f9ce63
sergihen7 <sergihen7.med@gmail.com>
2025-01-19 22:45:08 +0700

    Navbar Brand

--- app/Views/template/dashboard.php
+++ app/Views/template/dashboard.php
@@ -10,7 +10,9 @@ $baseUrl = "/web/admin";
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container">
-    <a class="navbar-brand" href="#">BarCode</a>
+    <a class="navbar-brand" href="<?= admin_url() ?>">
+      <img src="/img/logo-long.png" alt="Bootstrap" height="40">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
