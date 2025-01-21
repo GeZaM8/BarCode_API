de86114866872bf92b808a7abb1216bd00c6d54e
sergihen7 <sergihen7.med@gmail.com>
2025-01-21 17:36:40 +0700

    fix toast

--- public/scripts/toast-helper.js
+++ public/scripts/toast-helper.js
@@ -1,17 +1,17 @@
-const toastFailRequest = function (err) {
+const toastFailRequest = function (
+  err = "Failed to fetch data, please try again later."
+) {
   $.toast({
-    text:
-      err.responseJSON.message ??
-      "Failed to fetch data, please try again later.",
+    text: err.responseJSON.message,
     position: "bottom-right",
     bgColor: "#b21313",
     loader: false,
   });
 };
 
-toastSuccessRequest = function () {
+const toastSuccessRequest = function (msg = "Success fetch data.") {
   $.toast({
-    text: "Success fetch data.",
+    text: msg,
     position: "bottom-right",
     bgColor: "#4bb543",
     loader: false,
