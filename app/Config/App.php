c2613b1b1c760031787cbe92c7b508536f10177e
GezaM8 <sergeshehzaadm2@gmail.com>
2025-01-23 06:53:31 +0700

    publish in api

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'https://backend24.site/Rian/XI/barcode/qrcode/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
--- database/barcode_db.sql
+++ database/barcode_db.sql
@@ -26,9 +26,9 @@ CREATE TABLE IF NOT EXISTS `absensi` (
   PRIMARY KEY (`id_absensi`),
   KEY `FK_absensi_users` (`id_user`),
   CONSTRAINT `FK_absensi_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table barcode_db.absensi: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table barcode_db.jurusan
 CREATE TABLE IF NOT EXISTS `jurusan` (
@@ -37,13 +37,7 @@ CREATE TABLE IF NOT EXISTS `jurusan` (
   PRIMARY KEY (`kode_jurusan`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table barcode_db.jurusan: ~5 rows (approximately)
-REPLACE INTO `jurusan` (`kode_jurusan`, `nama_jurusan`) VALUES
-	('PH', 'Perhotelan'),
-	('RPL', 'Rekayasa Perangkat Lunak'),
-	('TBG', 'Tata Boga'),
-	('TBS', 'Tata Busana'),
-	('ULW', 'Usaha Layanan Pariwisata');
+-- Data exporting was unselected.
 
 -- Dumping structure for table barcode_db.kelas
 CREATE TABLE IF NOT EXISTS `kelas` (
@@ -52,23 +46,7 @@ CREATE TABLE IF NOT EXISTS `kelas` (
   PRIMARY KEY (`id_kelas`)
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table barcode_db.kelas: ~15 rows (approximately)
-REPLACE INTO `kelas` (`id_kelas`, `kelas`) VALUES
-	(1, '10 RPL 1'),
-	(2, '10 RPL 2'),
-	(3, '11 RPL 1'),
-	(4, '11 RPL 2'),
-	(5, '12 RPL 1'),
-	(6, '12 RPL 2'),
-	(7, '10 TBG 1'),
-	(8, '10 TBG 2'),
-	(9, '10 TBG 3'),
-	(10, '11 TBG 1'),
-	(11, '11 TBG 2'),
-	(12, '11 TBG 3'),
-	(13, '12 TBG 1'),
-	(14, '12 TBG 2'),
-	(15, '12 TBG 3');
+-- Data exporting was unselected.
 
 -- Dumping structure for table barcode_db.qrcode
 CREATE TABLE IF NOT EXISTS `qrcode` (
@@ -76,14 +54,9 @@ CREATE TABLE IF NOT EXISTS `qrcode` (
   `key_qrcode` varchar(255) NOT NULL,
   `tanggal_aktif` date NOT NULL,
   PRIMARY KEY (`id_qrcode`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table barcode_db.qrcode: ~0 rows (approximately)
-REPLACE INTO `qrcode` (`id_qrcode`, `key_qrcode`, `tanggal_aktif`) VALUES
-	(1, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNWVhOTk5YjEtY2M3OC0zZTU4LWI5MjAtMTEwN2RiYTk4NTM0IiwidGFuZ2dhbCI6IjIwMjUtMDEtMTIifQ.ftDPFjPgbJsRFstR7u6hoc7tp2DaOq3FcZbJGrdmjh4', '2025-01-12'),
-	(2, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiMjVhNjYwYmYtZDhmMy0zYmMxLWE1ZTQtNzI3Mjg4NjgwOTNmIiwidGFuZ2dhbCI6IjIwMjUtMDEtMTQifQ.Di330wzsNYzyZXp7A8RvGSfixar69brD1mtiBxLUDnQ', '2025-01-14'),
-	(3, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiOTA2M2NjNWYtNWRkNS0zOWE3LTkzOGEtMDFhOWU0MTJjN2NjIiwidGFuZ2dhbCI6IjIwMjUtMDEtMTkifQ.DV7eHeoin8Yoo_Bo0h3k--3CCnUVv6YXAF6Cq1NWh2g', '2025-01-19'),
-	(4, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiYmU4MjUwYjctMGM3My0zZTc5LWE4MTktYzk2ZDFmMTI3NWFhIiwidGFuZ2dhbCI6IjIwMjUtMDEtMjAifQ.hyY_MvecA-cxznzQGwGUWaliYcloO1TPLaqKp1B7j6o', '2025-01-20');
+-- Data exporting was unselected.
 
 -- Dumping structure for table barcode_db.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -97,14 +70,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   CONSTRAINT `FK_users_user_roles` FOREIGN KEY (`id_role`) REFERENCES `user_roles` (`id_role`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table barcode_db.users: ~6 rows (approximately)
-REPLACE INTO `users` (`id_user`, `email`, `password`, `id_role`) VALUES
-	(1, 'admin@email.com', '123', 3),
-	(2, 'aa@gmail.com', '123', 1),
-	(3, 'guru1@gmail.com', '123', 2),
-	(4, 'usea@gmail.com', '123', 1),
-	(13, 'user3@gmail.com', '123', 1),
-	(17, 'user4@gmail.com', '13', 1);
+-- Data exporting was unselected.
 
 -- Dumping structure for table barcode_db.user_roles
 CREATE TABLE IF NOT EXISTS `user_roles` (
@@ -113,11 +79,7 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
   PRIMARY KEY (`id_role`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table barcode_db.user_roles: ~2 rows (approximately)
-REPLACE INTO `user_roles` (`id_role`, `name_role`) VALUES
-	(1, 'Siswa'),
-	(2, 'Guru'),
-	(3, 'Admin');
+-- Data exporting was unselected.
 
 -- Dumping structure for table barcode_db.u_guru
 CREATE TABLE IF NOT EXISTS `u_guru` (
@@ -130,9 +92,7 @@ CREATE TABLE IF NOT EXISTS `u_guru` (
   CONSTRAINT `FK_u_guru_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table barcode_db.u_guru: ~0 rows (approximately)
-REPLACE INTO `u_guru` (`id_guru`, `id_user`, `nama`, `nip`) VALUES
-	(1, 3, 'Guru', '123');
+-- Data exporting was unselected.
 
 -- Dumping structure for table barcode_db.u_siswa
 CREATE TABLE IF NOT EXISTS `u_siswa` (
@@ -158,12 +118,7 @@ CREATE TABLE IF NOT EXISTS `u_siswa` (
   CONSTRAINT `u_siswa_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table barcode_db.u_siswa: ~4 rows (approximately)
-REPLACE INTO `u_siswa` (`id_siswa`, `id_user`, `nama`, `id_kelas`, `kode_jurusan`, `no_absen`, `nis`, `nisn`, `foto`) VALUES
-	(3, 2, 'tes', 1, 'RPL', '1', '1', 1, 'img/tes.png'),
-	(6, 4, 'a', 2, 'RPL', '', '2', 2, ''),
-	(7, 13, 'a', 1, 'RPL', '2', '3', 3, ''),
-	(10, 17, 'a', 1, 'RPL', '', '4', 4, '');
+-- Data exporting was unselected.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
