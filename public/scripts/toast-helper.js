c7df04af38caf6537216131b76077150d7230c64
sergihen7 <sergihen7.med@gmail.com>
2025-01-25 21:00:00 +0700

    Update Toas helper

--- public/scripts/toast-helper.js
+++ public/scripts/toast-helper.js
@@ -17,3 +17,23 @@ const toastSuccessRequest = function (msg = "Success fetch data.") {
     loader: false,
   });
 };
+
+const toastSuccessRequestTop = function (msg = "Success fetch data.") {
+  $.toast({
+    text: msg,
+    position: "top-center",
+    bgColor: "#4bb543",
+    loader: false,
+  });
+};
+
+const toastFailRequestTop = function (
+  err = "Failed to fetch data, please try again later."
+) {
+  $.toast({
+    text: err.responseJSON.message,
+    position: "top-center",
+    bgColor: "#b21313",
+    loader: false,
+  });
+};
