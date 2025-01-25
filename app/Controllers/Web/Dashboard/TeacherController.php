c45565c53afc05215ed223a0463855f94bd121ee
GezaM8 <sergeshehzaadm2@gmail.com>
2025-01-25 21:40:12 +0700

    update fix

--- app/Controllers/Web/Dashboard/TeacherController.php
+++ app/Controllers/Web/Dashboard/TeacherController.php
@@ -15,13 +15,14 @@ class TeacherController extends BaseController
 
     public function __construct()
     {
-        helper('weburl');
+        helper(['weburl', 'user']);
         $this->absenModel   = new \App\Models\Absensi();
         $this->kelasModel   = new \App\Models\Kelas();
         $this->userModel    = new \App\Models\User();
         $this->userRole     = new \App\Models\UserRole();
         $this->jurusanModel = new \App\Models\Jurusan();
     }
+
     public function index()
     {
         return view('dashboard/teacher_home', [
@@ -31,5 +32,52 @@ class TeacherController extends BaseController
             'kelas_count' => $this->kelasModel->countAllResults(),
             'jurusan_count' => $this->jurusanModel->countAllResults(),
         ]);
+    }
+
+    public function presence()
+    {
+        $data_yer = $this->absenModel->select("MIN(year(tanggal)) as start_year, MAX(year(tanggal)) as end_year")->first();
+        $class = $this->kelasModel->findAll();
+
+        return view('dashboard/teacher_presence', [
+            'title' => 'Teacher',
+            'current_page' => 'presence',
+            'start_year' => $data_yer->start_year ?? date("Y"),
+            'end_year' => $data_yer->end_year ?? date("Y"),
+            'class' => $class,
+        ]);
+    }
+
+    public function users()
+    {
+        return view('dashboard/teacher_users', [
+            'title' => 'Teacher',
+            'current_page' => 'data.users',
+            'roles' => $this->userRole->findAll(),
+            'kelas' => $this->kelasModel->orderBy("kelas", "ASC")->findAll(),
+            'jurusan' => $this->jurusanModel->orderBy("nama_jurusan", "ASC")->findAll(),
+        ]);
+    }
+
+    public function kelas()
+    {
+        return view('dashboard/teacher_kelas', [
+            'title' => 'Teacher',
+            'current_page' => 'data.kelas',
+        ]);
+    }
+
+    public function jurusan()
+    {
+        return view('dashboard/teacher_jurusan', [
+            'title' => 'Teacher',
+            'current_page' => 'data.jurusan',
+        ]);
+    }
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('/web');
     }
 }
