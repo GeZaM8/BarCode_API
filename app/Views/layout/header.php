7d63031e1c340dc0d7518871b2722e6f71baaa0c
sergihen7 <sergihen7.med@gmail.com>
2025-02-02 21:55:17 +0700

    👍🏻

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -9,3 +9,17 @@
     font-family: "Poppins", serif;
   }
 </style>
+
+<!-- //////////////////////////////////////////////////////////////////////
+  Authored By: sergihen7 and GeZaM8
+
+  Github:
+  https://github.com/sergihen7
+  https://github.com/GeZaM8
+
+  More:
+  IG: https://www.instagram.com/sergihen7/
+  YT: https://www.youtube.com/channel/UCAgV2n5Hw5TMhklqn-quvTA
+
+  Web: devcoga.biz.id
+////////////////////////////////////////////////////////////////////// -->
